bb83f7abe09e5a7301436aa82c08c9eb9960a8e6
Trevor Gudmundsen <trevor@ennetix.com>
2024-07-20 16:35:30 -0700

    Import PIL and pathlib for image file usage

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
+from PIL import Image
+from pathlib import Path
+
+photosDir = "C:\\Users\\trevo\\Downloads\\family-photos"
+
+result = list(Path(photosDir).rglob("*.jpg"))
+for each in result:
+    print(each)
+
 print("Hello, world!")
