82b73b6f09644af7e2b4a482e2303e476f830bfd
Trevor Gudmundsen <trevor@ennetix.com>
2024-07-20 17:38:34 -0700

    Add basic photo access and analysis

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # photomosaic
 Tool for creation of photo mosaic from a directory of photos
+
+### Install and run
+```powershell
+python3 -m venv .venv
+# May need to update execution policy in order to run the activate script:
+# Set-ExecutionPolicy RemoteSigned
+.\.venv\Scripts\activate
+python3 -m pip install --upgrade pip
+.\.venv\Scripts\pip install -r requirements.txt
+python3 ./src/main.py
+```
--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
 from PIL import Image
 from pathlib import Path
 
-photosDir = "C:\\Users\\trevo\\Downloads\\family-photos"
+photosDir = "C:\\Users\\trevo\\Downloads\\family-photos-clean"
 
-result = list(Path(photosDir).rglob("*.jpg"))
-for each in result:
-    print(each)
+photoPaths = list(Path(photosDir).rglob("*.jpg"))
+
+total = len(photoPaths)
+omitted = 0
+
+validPhotos = []
+for each in photoPaths:
+    im = Image.open(each)
+    w, h = im.size
+    if h > w:
+        omitted += 1
+    else:
+        validPhotos.append(each)
+
+im = Image.open(validPhotos[0])
+w, h = im.size
+print(w, h)
+print("total: ", total)
+print("omitted: ", omitted)
+
 
 print("Hello, world!")
