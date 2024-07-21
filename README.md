a0ef4caad2f099409f7c450d51aaa8af0e11aca2
Trevor Gudmundsen <trevor@ennetix.com>
2024-07-21 09:22:06 -0700

    Use pickle to persist indexed album metadata

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Tool for creation of photo mosaic from a directory of photos
 python3 -m venv .venv
 # May need to update execution policy in order to run the activate script:
 # Set-ExecutionPolicy RemoteSigned
-.\.venv\Scripts\activate
+.\.venv\Scripts\activate.ps1
 python3 -m pip install --upgrade pip
 .\.venv\Scripts\pip install -r requirements.txt
-python3 ./src/main.py
+.\.venv\Scripts\python ./src/main.py
 ```
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from PIL import Image
 from pathlib import Path
-import numpy
 import json
+# from types import SimpleNamespace
+import pickle
+
+import numpy
+from PIL import Image
 
 
 class PhotoMetadata:
@@ -16,6 +19,8 @@ class AlbumMetadata:
 def toJson(albumMeta: AlbumMetadata) -> str:
     return json.dumps(albumMeta, default = lambda o: o.__dict__)
 
+# def fromJson(jsonStr: str) -> AlbumMetadata:
+#     return json.loads(jsonStr, object_hook=lambda d: SimpleNamespace(**d))
 
 
 # Take a target aspect as float and dimensions as tuple (w, h)
@@ -47,7 +52,6 @@ def cropImage(image: Image, newDimensions) -> Image:
     return image.crop((0, 0, w, h))
 
 
-
 photosDir = "C:\\Users\\trevo\\Downloads\\family-photos-clean"
 # Use 3:2 aspect ratio.
 targetAspect = 1.5
@@ -56,7 +60,14 @@ aspectTolerance = 0.05
 # Each photo used will be scaled to this size.
 scaleSize = (750, 500)
 # The number of photos (across width, height) to create the final target photo.
-targetPhotoDimension = (30, 20)
+# Because initial test has all photos same dimension, the width and height will be the same.
+targetPhotoDimension = (32, 32)
+photoExtension = ".jpg"
+
+
+def getPhotoPaths(directory):
+    return list(Path(directory).rglob("*" + photoExtension))
+
 
 # Photo operations, using the first image as sample.
 photoPaths = list(Path(photosDir).rglob("*.jpg"))
@@ -73,37 +84,90 @@ newImg = cropImage(image, dimensions)
 # newImg.show()
 
 
-total = len(photoPaths)
-omitted = 0
+# Given the album path, create the album index/metadata and return as object.
+def indexAlbum(albumDir) -> AlbumMetadata:
+    photoPaths = getPhotoPaths(albumDir)
+    indices = []
+    count = 0
+    total = len(photoPaths)
+    omitted = 0
+    for path in photoPaths:
+        im = Image.open(path)
+        w, h = im.size
+        aspect = w / h
+        if abs(aspect - targetAspect) > aspectTolerance:
+            omitted += 1
+        else:
+            # For faster testing, only do 10 at a time for now.
+            if count > 10:
+                break
+            count = count + 1
+            print("handling valid photo: ", count)
+            meta = PhotoMetadata(path = path, avg_color = photoAvgColor(path))
+            indices.append(meta)
+    print("Total: ", total)
+    print("Omitted: ", omitted)
+    return AlbumMetadata(photos = indices)
+
+
+# def writeAlbumJsonFile(albumMeta, writePath):
+#     albumJson = toJson(albumMeta)
+#     f = open(writePath, "w")
+#     f.write(albumJson)
+
+def writeAlbumFile(albumMeta, writePath):
+    p = pickle.dumps(albumMeta)
+    f = open(writePath, "wb")
+    f.write(p)
+
+def readAlbumFile(readPath):
+    f = open(readPath, "rb")
+    p = f.read()
+    albumMeta = pickle.loads(p)
+    return albumMeta
+
+
+albumMeta = indexAlbum(photosDir)
+writeAlbumFile(albumMeta, "./meta.pickle")
+rAlbum = readAlbumFile("./meta.pickle")
+jsonStr = toJson(rAlbum)
+print(rAlbum)
+print(jsonStr)
 
 # Indexing photos to later generate a target photo.
 # Persistent indexing is for faster lookup, indexing 1000 photos takes a few mins.
-validPhotos = []
-indices = []
-count = 0
-for path in photoPaths:
-    im = Image.open(path)
-    w, h = im.size
-    aspect = w / h
-    if abs(aspect - targetAspect) > aspectTolerance:
-        omitted += 1
-    else:
-        # For faster testing, only do 10 at a time for now.
-        if count > 10:
-            break
-        count = count + 1
-        print("handling valid photo: ", count)
-        validPhotos.append(path)
-        meta = PhotoMetadata(path = path, avg_color = photoAvgColor(path))
-        indices.append(meta)
+# validPhotos = []
+# indices = []
+# count = 0
+# total = len(photoPaths)
+# omitted = 0
+# for path in photoPaths:
+#     im = Image.open(path)
+#     w, h = im.size
+#     aspect = w / h
+#     if abs(aspect - targetAspect) > aspectTolerance:
+#         omitted += 1
+#     else:
+#         # For faster testing, only do 10 at a time for now.
+#         if count > 10:
+#             break
+#         count = count + 1
+#         print("handling valid photo: ", count)
+#         validPhotos.append(path)
+#         meta = PhotoMetadata(path = path, avg_color = photoAvgColor(path))
+#         indices.append(meta)
 
-albumMeta = AlbumMetadata(photos = indices)
-albumJson = toJson(albumMeta)
-print(albumJson)
+# albumMeta = AlbumMetadata(photos = indices)
+# albumJson = toJson(albumMeta)
+# print(albumJson)
 
-print("total: ", total)
-print("omitted: ", omitted)
 
+
+# print("total: ", total)
+# print("omitted: ", omitted)
+
+
+# Photo album stats and analytics:
 # minH = float('inf')
 # maxH = 0
 # minW = float('inf')
@@ -137,6 +201,3 @@ print("omitted: ", omitted)
 # print("Width: ", minW, maxW)
 # print ("Avg Aspect: ", sumW / sumH)
 # print ("Aspect: ", minA, maxA)
-
-
-print("Hello, world!")
